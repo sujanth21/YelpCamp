faca943d7293c2387b75b863648886d5b87d4b7d
Sujanth <sujanth1987@gmail.com>
2017-03-08 19:34:40 +1100

    Comments styles completed

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var express     = require('express'),
 seedDB();
 mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 
