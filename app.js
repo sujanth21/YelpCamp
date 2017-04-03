61cc8479e46969989830091d4870d8b156cc7fe6
Sujanth <sujanth1987@gmail.com>
2017-04-03 22:20:49 +1000

    Edit and delete campgrounds added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express         = require('express'),
     mongoose        = require('mongoose'),
     passport        = require('passport'),
     LocalStrategy   = require('passport-local'),
+    methodOverride  = require('method-override'),
     Campground      = require('./models/campground'),
     Comment         = require('./models/comment'),
     User            = require('./models/user'),
@@ -22,6 +23,7 @@ mongoose.connect('mongodb://localhost/yelp_camp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
 
 //Passport Configuration
 app.use(session({
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -56,6 +56,39 @@ router.get('/:id', function(req, res){
     });
 });
 
+//Edit campgrounds
+router.get('/:id/edit', function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            res.redirect('/campgrounds');
+        } else {
+            res.render('campgrounds/edit', {campground: foundCampground});
+        }
+    });
+});
+
+//Update campgrounds
+router.put('/:id', function(req, res){
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+        if(err){
+            res.redirect('/campgrounds');
+        } else {
+            res.redirect('/campgrounds/' + req.params.id);
+        }
+    });
+});
+
+//Destroy campground router
+router.delete('/:id', function(req, res){
+    Campground.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect('/campgrounds');
+        } else {
+            res.redirect('/campgrounds');
+        }
+    });
+});
+
 //middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
