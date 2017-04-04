34720fd49bd49430a6105472b4f0db143c7a93c4
Sujanth <sujanth1987@gmail.com>
2017-04-04 21:37:13 +1000

    flash messages added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express         = require('express'),
     mongoose        = require('mongoose'),
     passport        = require('passport'),
     LocalStrategy   = require('passport-local'),
+    flash           = require('connect-flash'),
     methodOverride  = require('method-override'),
     Campground      = require('./models/campground'),
     Comment         = require('./models/comment'),
@@ -24,6 +25,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
+app.use(flash());
 
 //Passport Configuration
 app.use(session({
@@ -40,6 +42,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success  = req.flash('success');
     next();
 });
 
--- routes/comments.js
+++ routes/comments.js
@@ -26,6 +26,7 @@ router.post('/', middleware.isLoggedIn, function(req, res){
             //create new comments
             Comment.create(req.body.comment, function(err, comment){
                 if (err){
+                    req.flash('error', 'Something went wrong!');
                     console.log(err);
                 } else {
                     //add username and id to comment
@@ -36,6 +37,7 @@ router.post('/', middleware.isLoggedIn, function(req, res){
                     //connect new comment to campground
                     campground.comments.push(comment);
                     campground.save();
+                    req.flash('success', 'Successfully added comment!');
                     res.redirect('/campgrounds/' + campground._id);
                 }
             });
@@ -72,6 +74,7 @@ router.delete('/:comment_id', middleware.checkCommentOwnership, function(req, re
         if(err){
             res.redirect('back');
         } else {
+            req.flash('success', 'Successfully comment deleted!');
             res.redirect('/campgrounds/' + req.params.id);
         }
     });
